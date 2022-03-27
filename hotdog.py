dcf3960f2687070629f8b06ac95e1303d9873b72
slany3.jan <97552526+Jan-Slany@users.noreply.github.com>
2022-03-27 09:45:14 +0200

    final commit
    **Přidáno**
    - zlepšení uživatelského prostředí
    - možnost lehkého upravení ceny párku v kódu

--- hotdog.py
+++ hotdog.py
@@ -1,11 +1,50 @@
-x = float(input("Kč to Párek v rohlíku: "))
-print("")
+while True:
+	mena = input("Vyber měnu z které chtete převáďet [Kč/Párek v rohlíku] ").lower()
+	hodnota_parku = 20
 
-y = x*20
+	try:
+		if mena.startswith("k"):
+			pocet_kc = float(input("Zadejte počet Kč: "))
+			print("")
 
-if y == 1:
-	print("{} Párek v rohlíku".format(y))
-elif y == 2 or y == 3 or y == 4:
-	print("{} Párky v rohlíku".format(y))
-else:
-	print("{} Párků v rohlíku".format(y))
+			pocet_parku = pocet_kc/hodnota_parku
+
+			match pocet_parku:
+				case 1:
+					print("{} Párek v rohlíku".format(pocet_parku))
+					print("")
+				case 2, 3, 4:
+					print("{} Párky v rohlíku".format(pocet_parku))
+					print("")
+				case default:
+					print("{} Párků v rohlíku".format(pocet_parku))
+					print("")
+
+	except ValueError as e:
+		print("Zadej čislo")
+
+	except Exception as e:
+		print(e)
+
+	try:
+		if mena.startswith("p"):
+			pocet_parku = float(input("Zadejte počet Párků v rohlíku: "))
+			print("")
+
+			pocet_kc = pocet_parku*hodnota_parku
+
+			print("{} Kč".format(pocet_kc))
+			print("")
+
+	except ValueError as e:
+		print("Zadej čislo")
+
+	except Exception as e:
+		print(e)
+
+	again = input("Chcete převádět dál? ").lower()
+
+	if again != "ano":
+		break
+	else:
+		print("------------")
